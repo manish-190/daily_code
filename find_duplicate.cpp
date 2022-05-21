882257a4b2a672b6febba8fdc9c4d94d657a62eb
MANISH KUMAR <72985740+manish-190@users.noreply.github.com>
2022-05-21 16:47:21 +0530

    Add files via upload

---
+++ find_duplicate.cpp
@@ -0,0 +1,47 @@
+/*
+1,3,4,2,2  2 repete number
+this sole linera search
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    
+
+    // liner search use kare jab value  i == j hogi to dublicate mil jayega
+
+    // int a[] = {1, 3, 4, 2, 2}; // declare aray
+
+    // int n = sizeof(a) / sizeof(a[0]); // humne size nikla
+
+
+    // cout << "n is size" << n << endl; // size ko print kar diya  
+
+    // int count = 0;  // count liya kuku jab dublicate value milegee increase kar denge
+    // int dub_num = 0; // dublicate value store kara denge
+
+    // for (int i = 0; i < n; i++)   
+    // {
+
+    //     cout << "i run -->" << a[i] << endl;
+
+    //     for (int j = i + 1; j < n; j++)
+    //     {
+
+    //         cout << "j value run -->" << a[j] << endl;
+
+    //         if (a[i] == a[j])
+    //         {
+    //             count++;
+    //             dub_num = a[j];
+    //         }
+    //     }
+    // }
+
+    // if (count > 0)
+    // {
+    //     cout << dub_num << endl;
+    // }
+    
+
+}
